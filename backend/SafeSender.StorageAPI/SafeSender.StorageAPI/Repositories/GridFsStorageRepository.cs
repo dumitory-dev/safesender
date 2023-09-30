08aeabfbf88d4726de56428425fdf85761fb3b21
slonikappa <mkirn.dev@gmail.com>
2023-09-30 19:56:06 +0300

    Use GridFS to store files, add ability to store files in file system directly, update tests, update config

---
+++ backend/SafeSender.StorageAPI/SafeSender.StorageAPI/Repositories/GridFsStorageRepository.cs
@@ -0,0 +1,35 @@
+using MongoDB.Bson;
+using SafeSender.StorageAPI.Database;
+using SafeSender.StorageAPI.Interfaces;
+using SafeSender.StorageAPI.Models;
+
+namespace SafeSender.StorageAPI.Repositories;
+
+public class GridFsStorageRepository : IFilesRepository
+{
+    private readonly IMongoDbContext _context;
+
+    public GridFsStorageRepository(IMongoDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<byte[]> GetFileBytes(string fileHash)
+    {
+        var isVaildId = ObjectId.TryParse(fileHash, out var id);
+
+        if (!isVaildId)
+        {
+            return Array.Empty<byte>();
+        }
+        
+        return await _context.GridFSBucket.DownloadAsBytesAsync(id);
+    }
+
+    public async Task<FileSaveInfo> SaveFileBytes(string fileName, byte[] fileData)
+    {
+        var id = await _context.GridFSBucket.UploadFromBytesAsync(fileName, fileData);
+        
+        return new FileSaveInfo(id != ObjectId.Empty, id.ToString() ?? string.Empty);
+    }
+}
